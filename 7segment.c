bf20241db78deeea3b58a99eed9d501ce11c7c86
ThijsLaloo <thijs.laloo@gmail.com>
2021-02-20 17:36:46 +0100

    The functionality for a second 7-segment display has been added
    The functionality for a second 7-segment display has been added

--- 7segment.c
+++ 7segment.c
@@ -1,16 +1,18 @@
 #include "F28x_Project.h"
 #include "7segment.h"
 
-#define SEVENSEG_CLK_GPIO       4
-#define SEVENSEG_DIO_GPIO       3
+#define SEVENSEG_CLK_GPIO1      4
+#define SEVENSEG_DIO_GPIO1      3
+#define SEVENSEG_CLK_GPIO2      6
+#define SEVENSEG_DIO_GPIO2      5
 #define SEVENSEG_CLK_PERIOD     20 // µs
 #define SEVENSEG_NEG_IDX        16 // in sevenSeg_numberArray
 #define SEVENSEG_BLANK_IDX      17 // in sevenSeg_numberArray
 
-int sevenSeg_writeByte(char bData, bool biSendStop);
-int sevenSeg_writeStart();
-int sevenSeg_writeStop();
-int sevenSeg_readAck();
+int sevenSeg_writeByte(char bData, bool biSendStop, int iDispNumber);
+int sevenSeg_writeStart(int iDispNumber);
+int sevenSeg_writeStop(int iDispNumber);
+int sevenSeg_readAck(int iDispNumber);
 
 int sevenSeg_numberArray[] = { 0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, // 0..7
                                0x7F, 0x6f, 0x77, 0x7c, 0x58, 0x5e, 0x79, 0x71, // 8..F
@@ -21,6 +23,8 @@ int sevenSeg_numberArray[] = { 0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, /
 int sevenSeg_init()
 {
     EALLOW;
+
+    // Pin-setup for the first 7-segment display
 
     GpioCtrlRegs.GPAMUX1.bit.GPIO4 = 0;
     GpioCtrlRegs.GPAGMUX1.bit.GPIO4 = 0;
@@ -40,31 +44,54 @@ int sevenSeg_init()
     GpioCtrlRegs.GPAQSEL1.bit.GPIO4 = 3;  //Asynch input GPIO36
     GpioCtrlRegs.GPAQSEL1.bit.GPIO3 = 3;  //Asynch input GPIO37
 
+    // Pin-setup for the second 7-segment display
+
+    GpioCtrlRegs.GPAMUX1.bit.GPIO6 = 0;
+    GpioCtrlRegs.GPAGMUX1.bit.GPIO6 = 0;
+
+    GpioCtrlRegs.GPAMUX1.bit.GPIO5 = 0;
+    GpioCtrlRegs.GPAGMUX1.bit.GPIO5 = 0;
+
+    GpioCtrlRegs.GPAPUD.bit.GPIO6 = 0;  //disable pull-up
+    GpioCtrlRegs.GPAPUD.bit.GPIO5 = 0;  //disable pull-up
+
+    GpioCtrlRegs.GPAODR.bit.GPIO6 = 1;  //enable open-drain logic
+    GpioCtrlRegs.GPAODR.bit.GPIO5 = 1;  //enable open-drain logic
+
+    GpioCtrlRegs.GPADIR.bit.GPIO6 = 1;  //set GPIO as output
+    GpioCtrlRegs.GPADIR.bit.GPIO5 = 1;  //set GPIO as output
+
+    GpioCtrlRegs.GPAQSEL1.bit.GPIO6 = 3;  //Asynch input GPIOxx
+    GpioCtrlRegs.GPAQSEL1.bit.GPIO5 = 3;  //Asynch input GPIOxx
+
     EDIS;
 
-    GPIO_WritePin(SEVENSEG_CLK_GPIO, 1);
-    GPIO_WritePin(SEVENSEG_DIO_GPIO, 1);
+    GPIO_WritePin(SEVENSEG_CLK_GPIO1, 1);
+    GPIO_WritePin(SEVENSEG_DIO_GPIO1, 1);
+    GPIO_WritePin(SEVENSEG_CLK_GPIO2, 1);
+    GPIO_WritePin(SEVENSEG_DIO_GPIO2, 1);
 
-    sevenSeg_clear();
+    sevenSeg_clear(1);
+    sevenSeg_clear(2);
 
     return 0;
 }
 
-int sevenSeg_clear()
+int sevenSeg_clear(int iDispNumber)
 {
     int i;
-    sevenSeg_writeByte(0x40, true);
-    sevenSeg_writeByte(0xC0, false);
+    sevenSeg_writeByte(0x40, true, iDispNumber);
+    sevenSeg_writeByte(0xC0, false, iDispNumber);
     for (i = 0; i < 6; i++)
     {
-        sevenSeg_writeByte(0x00, false);
+        sevenSeg_writeByte(0x00, false, iDispNumber);
     }
-    sevenSeg_writeByte(0x00, true);
-    sevenSeg_writeByte(0x8B, true);
+    sevenSeg_writeByte(0x00, true, iDispNumber);
+    sevenSeg_writeByte(0x8B, true, iDispNumber);
     return 0;
 }
 
-int sevenSeg_writeTemp(int iTemp)
+int sevenSeg_writeTemp(int iTemp, int iDispNumber)
 {
     char baSegments[4] = { 0 };
     char bTemp = 0;
@@ -86,31 +113,31 @@ int sevenSeg_writeTemp(int iTemp)
     baSegments[2] = sevenSeg_numberArray[ones];
     baSegments[3] = sevenSeg_numberArray[thents];
 
-    sevenSeg_writeByte(0x40, true);
-    sevenSeg_writeByte(0xC0, false);
-    sevenSeg_writeByte(baSegments[0], false);
-    sevenSeg_writeByte(baSegments[1], false);
-    sevenSeg_writeByte(baSegments[2], false);
-    sevenSeg_writeByte(baSegments[3], true);
-    sevenSeg_writeByte(0x8B, true);
+    sevenSeg_writeByte(0x40, true, iDispNumber);
+    sevenSeg_writeByte(0xC0, false, iDispNumber);
+    sevenSeg_writeByte(baSegments[0], false, iDispNumber);
+    sevenSeg_writeByte(baSegments[1], false, iDispNumber);
+    sevenSeg_writeByte(baSegments[2], false, iDispNumber);
+    sevenSeg_writeByte(baSegments[3], true, iDispNumber);
+    sevenSeg_writeByte(0x8B, true, iDispNumber);
 
     return 0;
 }
 
-int sevenSeg_writeDisco()
+int sevenSeg_writeDisco(int iDispNumber)
 {
     int i, j;
     for (i = 0; i <= 0x10; i++)
     {
 
-        sevenSeg_writeByte(0x40, true);
-        sevenSeg_writeByte(0xC0, false);
+        sevenSeg_writeByte(0x40, true, iDispNumber);
+        sevenSeg_writeByte(0xC0, false, iDispNumber);
         for (j = 0; j <= 5; j++)
         {
-            sevenSeg_writeByte(sevenSeg_numberArray[i], false);
+            sevenSeg_writeByte(sevenSeg_numberArray[i], false, iDispNumber);
         }
-        sevenSeg_writeByte(sevenSeg_numberArray[i], true);
-        sevenSeg_writeByte(0x8B, true);
+        sevenSeg_writeByte(sevenSeg_numberArray[i], true, iDispNumber);
+        sevenSeg_writeByte(0x8B, true, iDispNumber);
         DELAY_US(1E5);
     }
     return 0;
@@ -118,69 +145,139 @@ int sevenSeg_writeDisco()
 
 // private functions
 
-int sevenSeg_writeStart()
+int sevenSeg_writeStart(int iDispNumber)
 {
+    if (iDispNumber == 1)
+    {
+        GPIO_WritePin(SEVENSEG_DIO_GPIO1, 0);
+    }
+    else if (iDispNumber == 2)
+    {
+        GPIO_WritePin(SEVENSEG_DIO_GPIO2, 0);
+    }
+    else
+    {
 
-    GPIO_WritePin(SEVENSEG_DIO_GPIO, 0);
+    }
     DELAY_US(SEVENSEG_CLK_PERIOD/4);
 
     return 0;
 }
 
-int sevenSeg_writeStop()
+int sevenSeg_writeStop(int iDispNumber)
 {
-    GPIO_WritePin(SEVENSEG_DIO_GPIO, 0);
-    GPIO_WritePin(SEVENSEG_CLK_GPIO, 0);
-    DELAY_US(SEVENSEG_CLK_PERIOD/2);
+    if (iDispNumber == 1)
+    {
+        GPIO_WritePin(SEVENSEG_DIO_GPIO1, 0);
+        GPIO_WritePin(SEVENSEG_CLK_GPIO1, 0);
+        DELAY_US(SEVENSEG_CLK_PERIOD/2);
 
-    GPIO_WritePin(SEVENSEG_CLK_GPIO, 1);
-    DELAY_US(SEVENSEG_CLK_PERIOD/2);
+        GPIO_WritePin(SEVENSEG_CLK_GPIO1, 1);
+        DELAY_US(SEVENSEG_CLK_PERIOD/2);
 
-    GPIO_WritePin(SEVENSEG_DIO_GPIO, 1);
-    DELAY_US(SEVENSEG_CLK_PERIOD/2);
+        GPIO_WritePin(SEVENSEG_DIO_GPIO1, 1);
+        DELAY_US(SEVENSEG_CLK_PERIOD/2);
+    }
+    else if (iDispNumber == 2)
+    {
+        GPIO_WritePin(SEVENSEG_DIO_GPIO2, 0);
+        GPIO_WritePin(SEVENSEG_CLK_GPIO2, 0);
+        DELAY_US(SEVENSEG_CLK_PERIOD/2);
+
+        GPIO_WritePin(SEVENSEG_CLK_GPIO2, 1);
+        DELAY_US(SEVENSEG_CLK_PERIOD/2);
+
+        GPIO_WritePin(SEVENSEG_DIO_GPIO2, 1);
+        DELAY_US(SEVENSEG_CLK_PERIOD/2);
+    }
+    else
+    {
+
+    }
 
     return 0;
 }
 
 int sevenSeg_readAck()
 {
-    int iRetValue = 0;
-    GPIO_WritePin(SEVENSEG_CLK_GPIO, 0);
-    DELAY_US(SEVENSEG_CLK_PERIOD/4);
+    if (iDispNumber == 1)
+    {
+        int iRetValue = 0;
+        GPIO_WritePin(SEVENSEG_CLK_GPIO1, 0);
+        DELAY_US(SEVENSEG_CLK_PERIOD/4);
 
-    iRetValue = GPIO_ReadPin(SEVENSEG_DIO_GPIO);
+        iRetValue = GPIO_ReadPin(SEVENSEG_DIO_GPIO1);
 
-    GPIO_WritePin(SEVENSEG_CLK_GPIO, 1);
-    DELAY_US(SEVENSEG_CLK_PERIOD/2);
+        GPIO_WritePin(SEVENSEG_CLK_GPIO1, 1);
+        DELAY_US(SEVENSEG_CLK_PERIOD/2);
+    }
+    else if (iDispNumber == 2)
+    {
+        int iRetValue = 0;
+        GPIO_WritePin(SEVENSEG_CLK_GPIO2, 0);
+        DELAY_US(SEVENSEG_CLK_PERIOD/4);
+
+        iRetValue = GPIO_ReadPin(SEVENSEG_DIO_GPIO2);
+
+        GPIO_WritePin(SEVENSEG_CLK_GPIO2, 1);
+        DELAY_US(SEVENSEG_CLK_PERIOD/2);
+    }
+    else
+    {
+
+    }
 
     return iRetValue;
 }
 
-int sevenSeg_writeByte(char bData, bool biSendStop)
+int sevenSeg_writeByte(char bData, bool biSendStop, int iDispNumber)
 {
 
-    sevenSeg_writeStart();
+    sevenSeg_writeStart(iDispNumber);
 
     int iBitCnt = 0;
 
-    for (iBitCnt = 0; iBitCnt < 8; ++iBitCnt)
+    if (iDispNumber == 1)
     {
-        GPIO_WritePin(SEVENSEG_CLK_GPIO, 0);
-        DELAY_US(SEVENSEG_CLK_PERIOD/4);
+        for (iBitCnt = 0; iBitCnt < 8; ++iBitCnt)
+        {
+            GPIO_WritePin(SEVENSEG_CLK_GPIO1, 0);
+            DELAY_US(SEVENSEG_CLK_PERIOD/4);
 
-        // (bData >> iBitCnt) & 0x01 =  telkens eerste bit afzonderen
+            // (bData >> iBitCnt) & 0x01 =  telkens eerste bit afzonderen
 
-        GPIO_WritePin(SEVENSEG_DIO_GPIO, (bData >> iBitCnt) & 0x01);
-        DELAY_US(SEVENSEG_CLK_PERIOD/4);
+            GPIO_WritePin(SEVENSEG_DIO_GPIO1, (bData >> iBitCnt) & 0x01);
+            DELAY_US(SEVENSEG_CLK_PERIOD/4);
 
-        GPIO_WritePin(SEVENSEG_CLK_GPIO, 1);
-        DELAY_US(SEVENSEG_CLK_PERIOD/2);
+            GPIO_WritePin(SEVENSEG_CLK_GPIO1, 1);
+            DELAY_US(SEVENSEG_CLK_PERIOD/2);
+        }
+    }
+    else if (iDispNumber == 2)
+    {
+        for (iBitCnt = 0; iBitCnt < 8; ++iBitCnt)
+        {
+            GPIO_WritePin(SEVENSEG_CLK_GPIO2, 0);
+            DELAY_US(SEVENSEG_CLK_PERIOD/4);
+
+            // (bData >> iBitCnt) & 0x01 =  telkens eerste bit afzonderen
+
+            GPIO_WritePin(SEVENSEG_DIO_GPIO2, (bData >> iBitCnt) & 0x01);
+            DELAY_US(SEVENSEG_CLK_PERIOD/4);
+
+            GPIO_WritePin(SEVENSEG_CLK_GPIO2, 1);
+            DELAY_US(SEVENSEG_CLK_PERIOD/2);
+        }
+    }
+    else
+    {
+
     }
 
-    sevenSeg_readAck();
+    sevenSeg_readAck(iDispNumber);
 
     if (biSendStop)
     {
-        sevenSeg_writeStop();
+        sevenSeg_writeStop(iDispNumber);
     }
 }
--- 7segment.h
+++ 7segment.h
@@ -3,9 +3,9 @@
 
 
 int sevenSeg_init();
-int sevenSeg_clear();
-int sevenSeg_writeTemp(int iTemp);
-int sevenSeg_writeDisco();
+int sevenSeg_clear(int iDispNumber);
+int sevenSeg_writeTemp(int iTemp, int iDispNumber);
+int sevenSeg_writeDisco(int iDispNumber);
 
 
 
