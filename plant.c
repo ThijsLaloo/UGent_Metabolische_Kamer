f38356746cf3486da98465bb39ee17232647c8b8
ThijsLaloo <thijs.laloo@gmail.com>
2021-02-22 14:40:35 +0100

    Update plant.c

--- plant.c
+++ plant.c
@@ -6,5 +6,12 @@
 
 int plant_refrigirate(bool biActive)
 {
+    int iReturnValue = 0;
 
+    if(biActive)
+        GpioDataRegs.GPASET.bit.PLANT_REFRIGITATECTRLPIN = 1;
+    else
+        GpioDataRegs.GPACLEAR.bit.PLANT_REFRIGITATECTRLPIN = 1;
+
+    return iReturnValue;
 }
